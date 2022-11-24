ea2f9111dacfe0c7e932dbc42f09c2773d5ce76b
IgorTeleychuk <teleychuk.i@yandex.r>
2022-11-24 15:10:52 +0300

    New project - CatsgramApplication

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.catsgram.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.service.PostService;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/posts")
+    public List<Post> findAll() {
+        return postService.findAll();
+    }
+
+    @PostMapping(value = "/post")
+    public Post create(@RequestBody Post post) {
+        return postService.create(post);
+    }
+
+    @GetMapping("/post/{postId}")
+    public Post findPost(@PathVariable("postId") Integer postId){
+        return postService.findPostById(postId);
+    }
+}
