4a4adc5a08a8ff55081ab409e8014ed5c40b8c5d
IgorTeleychuk <teleychuk.i@yandex.r>
2023-01-14 00:23:49 +0400

    Add: dao interface and implementation Refactoring: model, service, controllers

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -6,34 +6,19 @@ import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping
-    public Collection<User> findAll() {
-        return userService.findAll();
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
-    }
-
-    @PutMapping
-    public User updateUser(@RequestBody User user) {
-        return userService.updateUser(user);
-    }
-
-    @GetMapping("/user/{userMail}")
-    public User getUser(@PathVariable("userMail") String userMail){
-        return userService.findUserByEmail(userMail);
+    @GetMapping("/{login}")
+    public Optional<User> getUser(@PathVariable String login){
+        return userService.findUserById(login);
     }
 }
